c2780d2036a4d922eb87b9ec77e762a90397c3a6
TakoBigvava <bigvava0205@gmail.com>
2021-10-16 17:54:50 +0400

    feat:controls code

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -2,22 +2,26 @@ const imagesWrapper = document.querySelector(".image-wrapper");
 const images = document.getElementsByClassName("image");
 const width = images[0].clientWidth + 25; //25 is gap taken from .slider
 const texts = document.querySelectorAll("text");
+const buttons = document.querySelectorAll(".button");
 const circles = document.querySelectorAll(".circle");
 let counter = 0;
 let isEnabled = true;
 
 const slide = (e) => {
+    const index = getCircleIndex(e.target)
+    if(index ==counter){
+        isEnabled = true;
+        return
+    }
 	if (isEnabled) {
 		//check circle's index in order to slide left ot right
-        const index = getCircleIndex(e.target)
-		if (index == counter ) {
-			return;
-		} else if (index < counter) {
+        
+	     if (index < counter) {
 			imagesWrapper.style.transition = ".6s";
 			counter --;
 			imagesWrapper.style.transform = `translateX(${-width * index}px)`;
 			activeCircle(getCircleIndex(e.target));
-		} else {
+		} else if(index > counter) {
 			imagesWrapper.style.transition = ".6s";
 			counter ++;
 			imagesWrapper.style.transform = `translateX(${-width * index}px)`;
@@ -27,38 +31,52 @@ const slide = (e) => {
 	isEnabled = false;
 };
 
-circles.forEach((circle) => {
-	circle.addEventListener("click", slide);
+buttons.forEach((button) => {
+	button.addEventListener("click", slide);
+    
 });
 
 imagesWrapper.addEventListener("transitionend", () => {
+    isEnabled = true;
     if (counter > images.length - 1) {
 		counter  = 0;
 	} else if (counter < 0) {
 		counter = images.length - 1;
 	}
-
-	isEnabled = true;
 });
 
 function activeCircle(n) {
 	for (let circle of circles) {
 		circle.classList.remove("active-circle");
 	}
-	if (n > circles.length - 1) {
+	if (n > buttons.length - 1) {
 		n = 0;
 	} else if (n < 0) {
-		n = circles.length - 1;
+		n = buttons.length - 1;
 	}
 	circles[n].classList.add("active-circle");
 }
 
+//condition to distinguish if event target was list element or circle span element
 const getCircleIndex = function (element) {
-	let children = element.parentNode.parentNode.children;
-	for (let i = 0; i < children.length; i++) {
-		if (children[i] == element.parentNode) {
-			return i;
-		}
-	}
-	return -i;
+     //if event target is button element
+    if(element.classList.contains('button')){
+        let children = element.parentNode.children;
+        for (let i = 0; i < children.length; i++) {
+            if (children[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    //if event target is circle element
+else {
+        let children = element.parentNode.parentNode.children;
+        for (let i = 0; i < children.length; i++) {
+            if (children[i] == element.parentNode) {
+                return i;
+            }
+        }
+        return -1;
+    }
 };
